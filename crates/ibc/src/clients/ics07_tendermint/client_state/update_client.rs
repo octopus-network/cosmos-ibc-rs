b223a9e040da03361a05e40a70821e6fd24e4897
anonymousGiga <cryptonymGong@gmail.com>
2023-06-30 16:24:27 +0800

    add

--- crates/ibc/src/clients/ics07_tendermint/client_state/update_client.rs
+++ crates/ibc/src/clients/ics07_tendermint/client_state/update_client.rs
@@ -67,10 +67,10 @@ impl ClientState {
             };
 
             let options = self.as_light_client_options()?;
-            let mut now = ctx.host_timestamp()?.into_tm_time().unwrap();
+            // let now = ctx.host_timestamp()?.into_tm_time().unwrap();
             // ===================================================
             // for test
-            now = header.signed_header.header.time;
+            let now = header.signed_header.header.time;
             // ===================================================
 
             // main header verification, delegated to the tendermint-light-client crate.
